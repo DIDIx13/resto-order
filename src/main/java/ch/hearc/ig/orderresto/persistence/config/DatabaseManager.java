5e1b3d78450da27dcd4701b946880a5a79d8002a
CorsiReto <reto.corsi@he-arc.ch>
2024-11-01 16:05:52 +0100

    Created a DatabaseManager Class to Manage Connections

---
+++ src/main/java/ch/hearc/ig/orderresto/persistence/config/DatabaseManager.java
@@ -0,0 +1,43 @@
+package ch.hearc.ig.orderresto.persistence.config;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DatabaseManager {
+
+    public static Connection getConnection() throws SQLException {
+        return DatabaseConfig.getConnection(); // Usa il metodo da DatabaseConfig
+    }
+
+    public static void closeConnection(Connection conn) {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void closeStatement(Statement stmt) {
+        if (stmt != null) {
+            try {
+                stmt.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void closeResultSet(ResultSet rs) {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
