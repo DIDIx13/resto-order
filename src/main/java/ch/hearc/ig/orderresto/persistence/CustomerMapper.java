9619f12cde10ecbefa0b7785723791e6c754cdb1
CorsiReto <reto.corsi@he-arc.ch>
2024-11-12 21:08:59 +0100

    CustomerService

--- src/main/java/ch/hearc/ig/orderresto/persistence/CustomerMapper.java
+++ src/main/java/ch/hearc/ig/orderresto/persistence/CustomerMapper.java
@@ -1,13 +1,16 @@
 package ch.hearc.ig.orderresto.persistence;
 
 import ch.hearc.ig.orderresto.business.Customer;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Set;
 
 public interface CustomerMapper {
-    void addCustomer(Customer customer);
-    void removeCustomer(Customer customer);
-    void updateCustomer(Customer customer);
-    Customer findCustomerById(Long id);
-    Customer findCustomerByEmail(String email);
-    Set<Customer> findAllCustomers();
+    void addCustomer(Connection conn, Customer customer) throws SQLException;
+    void removeCustomer(Connection conn, Customer customer) throws SQLException;
+    void updateCustomer(Connection conn, Customer customer) throws SQLException;
+    Customer findCustomerById(Connection conn, Long id) throws SQLException;
+    Customer findCustomerByEmail(Connection conn, String email) throws SQLException;
+    Set<Customer> findAllCustomers(Connection conn) throws SQLException;
 }
